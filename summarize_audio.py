5c4ffca88b4a87856fa3653ed2f0535883aa85a4
CKnievel <cknievel@htwg-konstanz.de>
2024-10-08 16:29:25 +0200

    Refined audio summary

--- summarize_audio.py
+++ summarize_audio.py
@@ -11,72 +11,92 @@ parser = argparse.ArgumentParser(description="Convert speech to text and summari
 
 # Add the arguments
 parser.add_argument("-i", "--input", type=str, help="The path to the audio file to transcribe.")
-parser.add_argument("-o", "--output", type=str, help="The path to save the transcribed text.")
+parser.add_argument("-o", "--output", type=str, help="The path to save the summarized lecture content.")
+# Add the optional arguments
+parser.add_argument("-is", "--input_s", type=str, help="The path to the summary file. If summary file is given, audio input file will be ignored.")
 parser.add_argument("-mt", "--model_t", type=str, default="large", help="The model to use for transcription.")
-parser.add_argument("-st", "--save_t", type=str, help="Save the transcribed audio file to this path.")
-parser.add_argument("-ms", "--model_s", type=str, default="llama3.1:70b", help="The model to use for summarization.")
-
+parser.add_argument("-st", "--save_t", type=str, help="Save the transcribed audio file to this path. (Output of whisper)")
+parser.add_argument("-sst", "--save_st", type=str, default="summary.txt", help="Save the summarized transcription to this path.")
+parser.add_argument("-ms", "--model_s", type=str, default="gemma2:27b", help="The model to use for summarization.")
 
 # Parse the arguments
 args = parser.parse_args()
 
-# Load the model for transcription
-print("Loading model for transcription...", end="", flush=True)
-model = whisper.load_model(args.model_t)
-print("done")
+# Check for given summary file
+if not args.input_s:
+    # Load the model for transcription
+    print("Loading model for transcription...", end="", flush=True)
+    model = whisper.load_model(args.model_t)
+    print("done")
 
-# Transcribe the audio file
-print("Transcribing audio file...", end="", flush=True)
-result = model.transcribe(args.input)
-print("done")
+    # Transcribe the audio file
+    print("Transcribing audio file...", end="", flush=True)
+    result = model.transcribe(args.input)
+    print("done")
 
-# Saving the transcribed text to a file
-if args.save_t:
-    with open(args.save_t, "w", encoding="utf-8") as file:
-        file.write(result["text"])
+    # Saving the transcribed text to a file
+    if args.save_t:
+        with open(args.save_t, "w", encoding="utf-8") as file:
+            file.write(result["text"])
 
-# Split the text into chunks. Split at sentences or line breaks
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1024,
-    chunk_overlap=0,
-    length_function=len,
-    is_separator_regex=False,
-    separators=[
-        "\n\n",
-        "\n",
-        ".",
-    ],)
-sentences = splitter.split_text(result["text"])
+    # Split the text into chunks. Split at sentences or line breaks
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1024,
+        chunk_overlap=0,
+        length_function=len,
+        is_separator_regex=False,
+        separators=[
+            "\n\n",
+            "\n",
+            ".",
+        ],)
+    sentences = splitter.split_text(result["text"])
 
-# Summarize the text per chunk
-print("Loading model for summarization...", end="", flush=True)
-model = ChatOllama(
-            model=args.model_s,
-            temperature=0,
-            keep_alive=-1,
-            top_k=10,
-            top_p=0.5,
-        )
-print("done")
+    # Summarize the text per chunk
+    print("Loading model for summarization...", end="", flush=True)
+    model = ChatOllama(
+                model=args.model_s,
+                temperature=0.5,
+                keep_alive=-1,
+                top_k=10,
+                top_p=0.7,
+            )
+    print("done")
 
-query = """Please summarize the text. Stay as close to the original text as possible. Output only the summary.
+
+    query = """Please summarize the text. Stay as close to the original text as possible. Output only the summary.
         # Text
         {content}
         # Summary
         """
-prompt_template = PromptTemplate.from_template(template=query)
+    prompt_template = PromptTemplate.from_template(template=query)
 
-summaries = []
-print(f"Summarizing {len(sentences)} sentences...")
-for sentence in sentences:
-    print(f"Summary for sentence #{len(summaries)+1}...", end="", flush=True)
-    chain = prompt_template | model
-    summary = chain.invoke({"content": sentence})
-    
-    summaries.append(summary.content)
-    print("done")
+    summaries = []
+    print(f"Summarizing {len(sentences)} sentences...")
+    for sentence in sentences:
+        print(f"Summary for sentence #{len(summaries)+1}...", end="", flush=True)
+        chain = prompt_template | model
+        summary = chain.invoke({"content": sentence})
+        
+        summaries.append(summary.content)
+        print("done")
 
-# Save the summaries to output file   
-with open(args.output, "w", encoding="utf-8") as file:
-    for summary in summaries:
-        file.write(summary + "\n")
+    # Save the summaries to output file   
+    with open(args.save_st, "w", encoding="utf-8") as file:
+        for summary in summaries:
+            file.write(summary + "\n")
+else:
+    # Summary file is given, read the file
+    with open(args.input_s, "r", encoding="utf-8") as file:
+        summary = file.read()
+
+# Now provide a second summary of the entire text
+with open("prompts/summary_prompt.txt", "r") as file:
+    query = file.read()
+
+chain = PromptTemplate.from_template(template=query) | model
+summary = chain.invoke({"content": summary.content})
+
+# save summary to output file
+with open(args.output , "w", encoding="utf-8") as file:
+    file.write(summary.content)
